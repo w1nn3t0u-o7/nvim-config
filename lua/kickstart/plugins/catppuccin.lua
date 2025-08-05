b65ba505990c54207932952df60d9526c90a09d6
Mikolaj Zielniok <mik.ziel7890@gmail.com>
2025-08-05 19:51:45 +0200

    catppuccin theme cleanup

--- lua/kickstart/plugins/catppuccin.lua
+++ lua/kickstart/plugins/catppuccin.lua
@@ -1,30 +1,16 @@
 return {
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'catppuccin/nvim',
-    name = 'catppuccin',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('catppuccin').setup {
-        flavour = 'macchiato',
-        styles = {
-          comments = {}, -- Disable italics in comments
-        },
-        integrations = {
-          mason = true,
-          which_key = true,
-        },
-      }
-
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'catppuccin'
-    end,
-  },
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		---@diagnostic disable-next-line: missing-fields
+		require("catppuccin").setup({
+			flavour = "macchiato",
+			styles = {
+				comments = {}, -- Disable italics in comments
+			},
+			auto_integrations = true,
+		})
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
--- vim: ts=2 sts=2 sw=2 et
