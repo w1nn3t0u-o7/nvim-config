cc62425bc0aa509dddb66ce6dd1807995ad8e747
Mikolaj Zielniok <mik.ziel7890@gmail.com>
2025-09-27 19:33:16 +0200

    formatted plugin configs

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -3,7 +3,7 @@ return {
 	version = "*",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"nvim-tree/nvim-web-devicons",
 		"MunifTanjim/nui.nvim",
 	},
 	lazy = false,
